9aad8a56c55bfbdf87d52f3c1f3c8a1f578ef930
juanjosv98 <juanjosv98@gmail.com>
2025-12-11 15:38:43 +0100

    Style - adding static front-end prototype

--- src/main/java/thn/bme5/Main.java
+++ src/main/java/thn/bme5/Main.java
@@ -1,9 +1,13 @@
 package thn.bme5;
 
+import io.javalin.Javalin;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Javalin app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(8080);
     }
-
 }
